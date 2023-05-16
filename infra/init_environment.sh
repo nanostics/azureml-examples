8bad743550f4314434abf628a68ab63492cd08c1
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-16 14:45:08 -0600

    Add `SUBSCRIPTION_ID` and `REGISTRY_NAME` envs

--- infra/init_environment.sh
+++ infra/init_environment.sh
@@ -48,7 +48,7 @@ let "REGISTRY_TODAY=10#$(date +'%m%d')"
 let "REGISTRY_TOMORROW=10#$(date -d '+1 days' +'%m%d')"
 
 
-export LOCATION="East US"
+# export LOCATION="East US"
 export PREFIX=aml
 export SUFFIX=sdkv202
 export APP_NAME="github-sp-amlsdkv2-gh"
