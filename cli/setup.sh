837212b050992d4abab44adaf9438eb332f478f3
Joshua Ji <joshua.ji@jiwhiz.com>
2023-05-16 12:02:50 -0600

    Steps 3 and 4 of docs

--- cli/setup.sh
+++ cli/setup.sh
@@ -26,9 +26,9 @@ az extension add -n ml -y
 
 ## For backward compatibility - running on old subscription
 # <set_variables>
-GROUP="azureml-examples"
-LOCATION="eastus"
-WORKSPACE="main"
+GROUP="summerprojects2023"
+LOCATION="canadacentral"
+WORKSPACE="Nanostics_ML_Workspace"
 # </set_variables>
 
 # If RESOURCE_GROUP_NAME is empty, the az configure is pending.
